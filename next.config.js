03bcd6303a018e22b6bd9f9d8c23dbf9e0820726
SirDaechel <anayookpala26@gmail.com>
2024-01-26 05:53:40 +0100

    fixing bug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  target: "serverless",
+  target: "experimental-serverless-trace",
   images: {
     domains: ["razziwp.com"],
     remotePatterns: [
