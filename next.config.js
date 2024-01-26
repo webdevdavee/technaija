d72b185642c9accf86fa069d63d23cf0400af6f2
SirDaechel <anayookpala26@gmail.com>
2024-01-26 06:03:06 +0100

    test commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  target: "serverless",
-  target: "experimental-serverless-trace",
   images: {
     domains: ["razziwp.com"],
     remotePatterns: [
