13fc0b07f03a7bbd16c1c0c476eaf6c6d12c4164
SirDaechel <anayookpala26@gmail.com>
2024-02-07 15:47:24 +0100

    test commit

--- app/(root)/about-technaija/page.tsx
+++ app/(root)/about-technaija/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
             among a group of tech enthusiasts who saw an opportunity to bridge
             the gap between functionality and fashion in the world of phone
             accessories. Fueled by a desire to create products that resonate
-            with the vibrant spirit of Nigeria's tech-savvy community, we
+            with the vibrant spirit of Nigeria&apos;s tech-savvy community, we
             embarked on a journey to redefine the way people perceive phone
             cases.
           </p>
@@ -23,21 +23,21 @@ const page = () => {
             Technaija brand. Every phone case is meticulously crafted using the
             finest materials and undergoes rigorous quality control processes to
             ensure superior performance and longevity. We understand that your
-            phone is more than just a device, it's an extension of your
+            phone is more than just a device, it&apos;s an extension of your
             identity, and we strive to provide cases that not only protect but
             also enhance your mobile experience.
           </p>
           <p>
             <b>Global Reach, Local Roots</b> <br /> While Technaija is proud to
-            call Nigeria home, our reach extends far beyond its borders. We're
-            passionate about sharing the creativity and ingenuity of Nigerian
-            design with the world, and we're committed to making our products
-            accessible to customers around the globe. But no matter how far our
-            reach extends, we'll always stay true to our Nigerian roots, drawing
-            inspiration from the rich tapestry of culture, tradition, and
-            innovation that defines our homeland. Join us on our journey to
-            redefine the boundaries of phone protection and style. Embrace the
-            future with Technaija,
+            call Nigeria home, our reach extends far beyond its borders.
+            We&apos;re passionate about sharing the creativity and ingenuity of
+            Nigerian design with the world, and we&apos;re committed to making
+            our products accessible to customers around the globe. But no matter
+            how far our reach extends, we&apos;ll always stay true to our
+            Nigerian roots, drawing inspiration from the rich tapestry of
+            culture, tradition, and innovation that defines our homeland. Join
+            us on our journey to redefine the boundaries of phone protection and
+            style. Embrace the future with Technaija,
             <em>
               where innovation, sustainability, and community converge to create
               a brighter tomorrow for all.
