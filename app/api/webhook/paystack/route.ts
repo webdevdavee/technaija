a8f30c03eca33c3c299e7a27dfb7eaeff3f046f4
SirDaechel <anayookpala26@gmail.com>
2024-02-23 17:58:13 +0100

    test commit

--- app/api/webhook/paystack/route.ts
+++ app/api/webhook/paystack/route.ts
@@ -1,34 +1,40 @@
 import crypto from "crypto";
-import { NextApiRequest } from "next";
-import { NextApiResponse } from "next";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST" || req.url !== "/api/webhook/paystack") {
-    return res.status(404).send("Not found");
+// Get secret key from environment variables
+const secret = process.env.PAYSTACK_SECRET_KEY as string;
+
+// Define request and response types
+type Request = {
+  method: string;
+  body: any;
+  headers: {
+    [key: string]: string;
+  };
+};
+
+type Response = {
+  status: (code: number) => Response;
+  end: () => void;
+};
+
+// Export default function for nextjs server
+export default function handler(req: Request, res: Response) {
+  // Check if request method is POST
+  if (req.method === "POST") {
+    // Validate event
+    const hash = crypto
+      .createHmac("sha512", secret)
+      .update(JSON.stringify(req.body))
+      .digest("hex");
+    if (hash == req.headers["x-paystack-signature"]) {
+      // Retrieve the request's body
+      const event = req.body;
+      // Do something with event
+    }
+    // Send 200 response
+    res.status(200).end();
+  } else {
+    // Send 405 response for other methods
+    res.status(405).end();
   }
-
-  const secret = process.env.PAYSTACK_SECRET_KEY as string;
-
-  if (!secret) {
-    throw new Error("Missing SECRET_KEY environment variable");
-  }
-
-  const hash = crypto
-    .createHmac("sha512", secret)
-    .update(JSON.stringify(req.body))
-    .digest("hex");
-
-  if (hash !== req.headers["x-paystack-signature"]) {
-    return res.status(401).send("Unauthorized");
-  }
-
-  const event = req.body;
-
-  // Do something with event
-  console.log("Received webhook event:", event); // Example usage
-
-  res.status(200).send("OK");
 }
--- userID.ts
+++
@@ -1,1 +0,0 @@
-export const currentUserID = "65bc9b8d3a3e01046de4fb44";
