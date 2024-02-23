5b16a5b9f626d39dbb869e14a6afc4284a1b8569
SirDaechel <anayookpala26@gmail.com>
2024-02-23 18:29:07 +0100

    test commit

--- app/api/webhook/paystack/route.ts
+++ app/api/webhook/paystack/route.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export function POST(req: NextApiRequest, res: NextApiResponse) {
   // Retrieve the request's body
   const event = req.body;
 
   // Do something with event
-  res.status(200).send("OK");
+  return new Response("", { status: 200 });
 }
