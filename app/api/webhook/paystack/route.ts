9e230a254effc2b6b43aeadf8ba1150334c10d75
SirDaechel <anayookpala26@gmail.com>
2024-02-24 11:45:23 +0100

    test commit

--- app/api/webhook/paystack/route.ts
+++ app/api/webhook/paystack/route.ts
@@ -1,9 +1,48 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export function POST(req: NextRequest, res: NextResponse) {
-  // Retrieve the request's body
-  const event = req.body;
+// Import the crypto module
+import crypto from "crypto";
 
-  // Do something with event
-  return new Response("", { status: 200 });
+const secret = process.env.PAYSTACK_SECRET_KEY as string;
+
+// Define the API route handler
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Check if the request method is POST
+  if (req.method === "POST") {
+    // Validate the event
+    const hash = crypto
+      .createHmac("sha512", secret)
+      .update(JSON.stringify(req.body))
+      .digest("hex");
+
+    if (hash === req.headers["x-paystack-signature"]) {
+      // Retrieve the request body
+      const event = req.body;
+      // Do something with the event
+      if (event && event.event === "transfer.success") {
+        return res.status(200).json({ message: "Transfer successful" });
+      }
+    }
+
+    // Send a 200 response
+    res.send(200);
+  } else {
+    // Send a 405 response for other methods
+    res.status(405).send("Method Not Allowed");
+  }
 }
+
+// Paystack Webhook function ends here
+
+// import { NextRequest, NextResponse } from "next/server";
+
+// export function POST(req: NextRequest, res: NextResponse) {
+//   // Retrieve the request's body
+//   const event = req.body;
+
+//   // Do something with event
+//   return new Response("", { status: 200 });
+// }
