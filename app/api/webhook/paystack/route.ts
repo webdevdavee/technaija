1c2e438a2aadeade06a4eb78781882b3166dc15f
SirDaechel <anayookpala26@gmail.com>
2024-02-24 14:33:10 +0100

    test commit

--- app/api/webhook/paystack/route.ts
+++ app/api/webhook/paystack/route.ts
@@ -1,5 +1,7 @@
-import { NextResponse } from "next/server";
 import crypto from "crypto";
+import { NextResponse } from "next/server";
+
+const secret = process.env.PAYSTACK_SECRET_KEY as string;
 
 interface HeadersWithSignature extends Headers {
   "x-paystack-signature": string;
@@ -9,39 +11,79 @@ interface PaystackEvent {
   event: string;
 }
 
-const secret = process.env.PAYSTACK_SECRET_KEY as string;
-
-export function POST(req: Request) {
-  // Check if the request method is POST
-  if (req.method === "POST") {
-    // Validate the event
-    const hash = crypto
-      .createHmac("sha512", secret)
-      .update(JSON.stringify(req.body))
-      .digest("hex");
-
-    const headers = req.headers as HeadersWithSignature;
-
-    if (hash === headers["x-paystack-signature"]) {
-      // Retrieve the request body
-      const event = req.body as unknown as PaystackEvent;
-      // Do something with the event
-      if (event && event.event === "charge.success") {
-        console.log("Transfer successful");
-        return NextResponse.json({
-          message: "Transfer successful",
-          event: event,
-        });
-      }
-    }
-
-    // Send a 200 response
-    return new Response("", { status: 200 });
-  } else {
-    // Send a 405 response for other methods
-    return new Response("Method Not Allowed", { status: 405 });
-  }
+function verify(eventData: any, signature: string): boolean {
+  const hmac = crypto.createHmac("sha512", secret);
+  const expectedSignature = hmac
+    .update(JSON.stringify(eventData))
+    .digest("hex");
+  return expectedSignature === signature;
 }
+
+export function handler(req: Request) {
+  const headers = req.headers as HeadersWithSignature;
+  const eventData = req.body as unknown as PaystackEvent;
+  const signature = headers["x-paystack-signature"];
+
+  if (!verify(eventData, signature)) {
+    return new Response("", { status: 400 });
+  }
+
+  if (eventData?.event === "charge.success") {
+    // Process the successful transaction to maybe fund wallet and update your WalletModel
+    console.log(`Transaction ${eventData.event} was successful`);
+    return NextResponse.json({
+      message: "Transfer successful",
+      event: eventData,
+    });
+  }
+
+  return new Response("", { status: 200 });
+}
+
+// import { NextResponse } from "next/server";
+// import crypto from "crypto";
+
+// interface HeadersWithSignature extends Headers {
+//   "x-paystack-signature": string;
+// }
+
+// interface PaystackEvent {
+//   event: string;
+// }
+
+// const secret = process.env.PAYSTACK_SECRET_KEY as string;
+
+// export function POST(req: Request) {
+//   // Check if the request method is POST
+//   if (req.method === "POST") {
+//     // Validate the event
+//     const hash = crypto
+//       .createHmac("sha512", secret)
+//       .update(JSON.stringify(req.body))
+//       .digest("hex");
+
+//     const headers = req.headers as HeadersWithSignature;
+
+//     if (hash === headers["x-paystack-signature"]) {
+//       // Retrieve the request body
+//       const event = req.body as unknown as PaystackEvent;
+//       // Do something with the event
+//       if (event && event.event === "charge.success") {
+//         console.log("Transfer successful");
+//         return NextResponse.json({
+//           message: "Transfer successful",
+//           event: event,
+//         });
+//       }
+//     }
+
+//     // Send a 200 response
+//     return new Response("", { status: 200 });
+//   } else {
+//     // Send a 405 response for other methods
+//     return new Response("Method Not Allowed", { status: 405 });
+//   }
+// }
 
 // Paystack Webhook function ends here
 
