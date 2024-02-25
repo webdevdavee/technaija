aef44d10d7742fa3960904a801f1637b2745fec3
SirDaechel <anayookpala26@gmail.com>
2024-02-25 19:32:59 +0100

    test commit

--- app/api/webhook/paystack/route.js
+++ app/api/webhook/paystack/route.js
@@ -1,3 +1,22 @@
+import crypto from "crypto";
+const secret = process.env.PAYSTACK_SECRET_KEY;
+
+export default function handler(req, res) {
+  // Validate event
+  const hash = crypto
+    .createHmac("sha512", secret)
+    .update(JSON.stringify(req.body))
+    .digest("hex");
+  if (hash == req.headers["x-paystack-signature"]) {
+    // Retrieve the request's body
+    const event = req.body;
+    if (event?.event === "charge.success") {
+      console.log("Transfer successful");
+    }
+  }
+  res.status(200).send("OK");
+}
+
 // import { NextResponse } from "next/server";
 // import crypto from "crypto";
 
@@ -45,20 +64,20 @@
 
 // Paystack Webhook function ends here
 
-import { NextRequest, NextResponse } from "next/server";
-interface PaystackEvent {
-  event: string;
-}
+// import { NextRequest, NextResponse } from "next/server";
+// interface PaystackEvent {
+//   event: string;
+// }
 
-export function POST(req: NextRequest, res: NextResponse) {
-  // Retrieve the request's body
-  const event = req.body as unknown as PaystackEvent;
-  if (event?.event === "charge.success") {
-    console.log("Transfer successful");
-    return NextResponse.json({
-      message: "Transfer successful",
-      event: event,
-    });
-  }
-  return new Response("", { status: 200 });
-}
+// export function POST(req: NextRequest, res: NextResponse) {
+//   // Retrieve the request's body
+//   const event = req.body as unknown as PaystackEvent;
+//   if (event?.event === "charge.success") {
+//     console.log("Transfer successful");
+//     return NextResponse.json({
+//       message: "Transfer successful",
+//       event: event,
+//     });
+//   }
+//   return new Response("", { status: 200 });
+// }
