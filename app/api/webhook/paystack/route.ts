cfd042adf58ebd2a2a5b3077ca0dcc04b46e71e1
SirDaechel <anayookpala26@gmail.com>
2024-02-26 09:24:31 +0100

    test commit

---
+++ app/api/webhook/paystack/route.ts
@@ -0,0 +1,67 @@
+import { NextResponse } from "next/server";
+import crypto from "crypto";
+
+interface HeadersWithSignature extends Headers {
+  "x-paystack-signature": string;
+}
+
+interface PaystackEvent {
+  event: string;
+}
+
+const secret = process.env.PAYSTACK_SECRET_KEY as string;
+
+export function POST(req: Request) {
+  // Check if the request method is POST
+  if (req.method === "POST") {
+    // Validate the event
+    const hash = crypto
+      .createHmac("sha512", secret)
+      .update(JSON.stringify(req.body))
+      .digest("hex");
+
+    const headers = req.headers as HeadersWithSignature;
+
+    if (hash === headers["x-paystack-signature"]) {
+      // Retrieve the request body
+      const event = req.body as unknown as PaystackEvent;
+      // Do something with the event
+      if (event && event.event === "charge.success") {
+        console.log("Transfer successful");
+        return NextResponse.json({
+          message: "Transfer successful",
+          event: event,
+        });
+      }
+      console.log("Outside event");
+    }
+
+    console.log("Outside hash");
+
+    // Send a 200 response
+    return new Response("", { status: 200 });
+  } else {
+    // Send a 405 response for other methods
+    return new Response("Method Not Allowed", { status: 405 });
+  }
+}
+
+// Paystack Webhook function ends here
+
+// import { NextRequest, NextResponse } from "next/server";
+// interface PaystackEvent {
+//   event: string;
+// }
+
+// export function POST(req: NextRequest, res: NextResponse) {
+//   // Retrieve the request's body
+//   const event = req.body as unknown as PaystackEvent;
+//   if (event?.event === "charge.success") {
+//     console.log("Transfer successful");
+//     return NextResponse.json({
+//       message: "Transfer successful",
+//       event: event,
+//     });
+//   }
+//   return new Response("", { status: 200 });
+// }
