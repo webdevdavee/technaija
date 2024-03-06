67ed6b8e86267d5f6235c3e34651b92ad0b7b43f
SirDaechel <anayookpala26@gmail.com>
2024-03-06 08:22:59 +0100

    improved functionality

--- app/api/webhook/paystack/route.ts
+++ app/api/webhook/paystack/route.ts
@@ -28,7 +28,7 @@ export async function POST(req: Request, res: Response) {
         email: event.data.customer.email,
         amount: event.data.amount / 100,
         products: event.data.metadata.userCart,
-        date: new Date(event.data.created_at),
+        date: event.data.paid_at,
         status: event.data.status,
         channel: event.data.channel,
       };
