a171868be5567fc6aa3282a53c022e6b09e7e199
SirDaechel <anayookpala26@gmail.com>
2024-04-09 12:13:57 +0100

    fixed bug

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -56,27 +56,14 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
-    let user;
-
-    {
-      last_name
-        ? (user = {
-            clerkId: id,
-            email: email_addresses[0].email_address,
-            username: username!,
-            firstName: first_name,
-            lastName: last_name,
-            photo: image_url,
-          })
-        : (user = {
-            clerkId: id,
-            email: email_addresses[0].email_address,
-            username: username!,
-            firstName: first_name,
-            lastName: first_name,
-            photo: image_url,
-          });
-    }
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name ? last_name : first_name,
+      photo: image_url,
+    };
 
     const newUser = await createUser(user);
 
