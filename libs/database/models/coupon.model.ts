cf0a44d89220fdd5a21caa81fa3f879e2fd5b3d4
SirDaechel <anayookpala26@gmail.com>
2024-04-11 20:00:46 +0100

    improvements

--- libs/database/models/coupon.model.ts
+++ libs/database/models/coupon.model.ts
@@ -3,7 +3,7 @@ import { Schema, model, models } from "mongoose";
 const CouponSchema = new Schema({
   code: { type: String, required: true },
   discount: { type: Number, required: true },
-  limit: { type: Number, required: true },
+  limit: { type: Number },
 });
 
 const Coupons = models.Coupons || model("Coupons", CouponSchema);
